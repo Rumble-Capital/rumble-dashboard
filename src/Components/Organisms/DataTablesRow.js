910fdee51b71d81f3a71160e5c0e145bbe478c42
ChrisCruze <cruzc09@gmail.com>
2020-12-23 18:52:00 -0500

    updated dashboard to include datatable

--- src/Components/Organisms/DataTablesRow.js
+++ src/Components/Organisms/DataTablesRow.js
@@ -7,25 +7,18 @@ import DataTableAsanaCurrent from "../Molecules/DataTableAsanaCurrent";
 import DataTableAirTableGoals from "../Molecules/DataTableAirTableGoals";
 import DataTableAirTableExperiments from "../Molecules/DataTableAirTableExperiments";
 
-const DataTablesRow = ({ toggl_list, airtable_list, asana_list }) => {
+const DataTablesRow = () => {
   return (
     <PropsContext.Consumer>
-      {({
-        airtable_expenses_list,
-        toggl_list,
-        airtable_toggl_list,
-        asana_list,
-        shopify_orders,
-        shopify_events,
-        shopify_customers,
-        shopify_products,
-        airtable_experiments_list,
-        airtable_goals_list,
-        refreshToggl,
-        refreshAsana,
-        monday_list
-      }) => {
-        console.log({ asana_list });
+      {({ firebase_list }) => {
+        const firebase_emails = firebase_list.filter(function(D) {
+          return D.email != "null";
+        });
+        const firebase_clicks = firebase_list.filter(function(D) {
+          return D.click == true;
+        });
+
+        console.log({ firebase_emails, firebase_clicks });
         return (
           <div class="row">
             <div className="ibox float-e-margins">
@@ -33,139 +26,54 @@ const DataTablesRow = ({ toggl_list, airtable_list, asana_list }) => {
                 <div className="tabs-container">
                   <ul class="nav nav-tabs">
                     <li class="active">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-monday-completed">
-                        Completed (
+                      <a aria-expanded="true" data-toggle="tab" href="#tab-firebase-visits">
+                        Visits (
                         {
-                          monday_list.filter(function(D) {
-                            return D.status == "Done";
-                          }).length
-                        }
-                        )
-                      </a>
-                    </li>
-                    <li class="active">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-monday">
-                        Current (
-                        {
-                          monday_list.filter(function(D) {
-                            return D.status != "Done";
+                          firebase_list.filter(function(D) {
+                            return true;
                           }).length
                         }
                         )
                       </a>
                     </li>
                     <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-asana-completed">
-                        Completed (
-                        {
-                          asana_list.filter(function(D) {
-                            return D.completed == true;
-                          }).length
-                        }
-                        )
+                      <a aria-expanded="true" data-toggle="tab" href="#tab-firebase-emails">
+                        Emails ({firebase_emails.length})
                       </a>
                     </li>
                     <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-asana">
-                        Current (
-                        {
-                          asana_list.filter(function(D) {
-                            return D.completed != true;
-                          }).length
-                        }
-                        )
+                      <a aria-expanded="true" data-toggle="tab" href="#tab-firebase-clicks">
+                        Clicks ({firebase_clicks.length})
                       </a>
                     </li>
-                    <li class="">
-                      <a aria-expanded="false" data-toggle="tab" href="#tab-expenses">
-                        Expenses ({airtable_expenses_list.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="false" data-toggle="tab" href="#tab-goals">
-                        Goals ({airtable_goals_list.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="false" data-toggle="tab" href="#tab-experiments">
-                        Experiments ({airtable_experiments_list.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="false" data-toggle="tab" href="#tab-toggl">
-                        Toggl ({toggl_list.length})
-                      </a>
-                    </li>
-                    {/* <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-shopify_orders">
-                        Orders ({shopify_orders.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-shopify_events">
-                        Events ({shopify_events.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-shopify_customers">
-                        Customers ({shopify_customers.length})
-                      </a>
-                    </li>
-                    <li class="">
-                      <a aria-expanded="true" data-toggle="tab" href="#tab-shopify_products">
-                        Products ({shopify_products.length})
-                      </a>
-                    </li> */}
                   </ul>
                   <div className="tab-content">
-                    {/* <div className="tab-pane active" id="tab-8">
-                    <div className="panel-body active">
-                      <DataTablesTable
-                        {...{
-                          id: "asana_table",
-                          idSrc: "gid",
-                          fields: [{ name: "gid", label: "gid" }, { name: "assignee", label: "assignee" }],
-                          data: asana_list
-                        }}
-                      />
-                    </div>
-                  </div> */}
-                    <div className="tab-pane active" id="tab-asana-completed">
+                    <div className="tab-pane active" id="tab-firebase-visits">
                       <div className="panel-body active">
-                        <DataTableAsanaCompleted list={asana_list} />
-                      </div>
-                    </div>
-                    <div className="tab-pane" id="tab-asana">
-                      <div className="panel-body">
-                        <DataTableAsanaCurrent list={asana_list} />
-                      </div>
-                    </div>
-                    <div className="tab-pane " id="tab-toggl">
-                      <div className="panel-body">
                         <DataTable
                           {...{
-                            id: "toggl_table",
-                            data: toggl_list
+                            id: "firebase_visits_table",
+                            data: firebase_list
                           }}
                         />
                       </div>
                     </div>
-                    <div className="tab-pane " id="tab-experiments">
-                      <div className="panel-body">
-                        <DataTableAirTableExperiments list={airtable_experiments_list} />
-                      </div>
-                    </div>
-                    <div className="tab-pane " id="tab-goals">
-                      <div className="panel-body">
-                        <DataTableAirTableGoals list={airtable_goals_list} />
-                      </div>
-                    </div>
-                    <div className="tab-pane " id="tab-expenses">
+                    <div className="tab-pane" id="tab-firebase-emails">
                       <div className="panel-body">
                         <DataTable
                           {...{
-                            id: "expenses_table",
-                            data: airtable_expenses_list
+                            id: "firebase_emails_table",
+                            data: firebase_emails
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <div className="tab-pane " id="tab-firebase-clicks">
+                      <div className="panel-body">
+                        <DataTable
+                          {...{
+                            id: "firebase_clicks_table",
+                            data: firebase_clicks
                           }}
                         />
                       </div>
--- src/Components/Templates/HomeTemplate.js
+++ src/Components/Templates/HomeTemplate.js
@@ -14,6 +14,7 @@ const DashboardTemplate = () => {
         <div className="wrapper wrapper-content">
           <MetricsRow />
           <ChartsRow />
+          <DataTablesRow />
         </div>
       </div>
     </div>
