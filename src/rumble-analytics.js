40e2f9c2b7845d4bbec6942bea44f217fe5d24d0
ChrisCruze <cruzc09@gmail.com>
2020-12-27 14:19:55 -0500

    updated firebase to save to different database

--- src/rumble-analytics.js
+++ src/rumble-analytics.js
@@ -1,24 +1,25 @@
 
 var firebaseConfig = {
-apiKey: "AIzaSyDzF8wXH6JSLeW0m5HphLme0V7nV15srrk",
-authDomain: "rumble-signup.firebaseapp.com",
-databaseURL: "https://rumble-signup.firebaseio.com",
-projectId: "rumble-signup",
-storageBucket: "rumble-signup.appspot.com",
-messagingSenderId: "142131072925",
-appId: "1:142131072925:web:fdd8a9ed39d5f7b4e46974",
-measurementId: "G-RDXNCRB8HG"
+  apiKey: "AIzaSyCFz1a7HsKrolW9nvVTzs_usvpbUBNPSj0",
+  authDomain: "rumble-2f0a7.firebaseapp.com",
+  databaseURL: "https://rumble-2f0a7.firebaseio.com",
+  projectId: "rumble-2f0a7",
+  storageBucket: "rumble-2f0a7.appspot.com",
+  messagingSenderId: "159695269471",
+  appId: "1:159695269471:web:caac783e8c987ed3eef5ea",
+  measurementId: "G-L9JZ6KB054"
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-var firebase_ref = firebase.database().ref();
-const visit_id = moment().unix()
+var firebase_ref = firebase.database().ref("visits");
+var visit_id = moment().unix()
+var time = Date();
 
 
 function visit_save_firebase() {
   $.getJSON("https://ipapi.co/json/", function(response) {
-    const response_with_url = Object.assign({}, response, { url: document.URL, time_stamp: moment().format() , email: ''});
+    const response_with_url = Object.assign({}, response, { url: document.URL, time_stamp: moment().format() , email: 'null',time:time});
     firebase_ref.child(visit_id).set(response_with_url)
   });
 }
@@ -29,7 +30,7 @@ visit_save_firebase()
 $("#email_button").on("click", function() {
   var email = $("#email_input").val();
   $.getJSON("https://ipapi.co/json/", function(response) {
-    const response_with_url = Object.assign({}, response, { url: document.URL, time_stamp: moment().format() , email: email});
+    const response_with_url = Object.assign({}, response, { url: document.URL, time_stamp: moment().format() , email: email,time:time});
     firebase_ref.child(visit_id).set(response_with_url)
   });
 	$("#email_input").val("");
